ebb7a79dfb0580c3abc815214f870186be487bd6
PemulaBanget123 <164504353+PemulaBanget123@users.noreply.github.com>
2024-04-17 20:52:21 +0700

    profilescript.js

--- profilescript.js
+++ profilescript.js
@@ -38,7 +38,49 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (guidesButton) {
         guidesButton.addEventListener('click', function() {
-            window.location.href = 'Guides.html';
+            window.location.href = 'News.html';
         });
     }
 });
+
+function deletepop(){
+    let viewlist = document.getElementById("ViewList");
+    let title1 = document.getElementById("title1");
+    let popup4 = document.getElementById("popup4");
+    let isiprofile = document.getElementById("isiprofile");
+    bg.style.filter = "blur(10px)";
+    title1.style.filter = "blur(10px)";
+    isiprofile.style.filter = "blur(10px)";
+    thirdbutton.style.filter = "blur(0)";
+    viewlist.style.filter = "blur(10px)";
+    popup4.classList.add("open-popup4");
+    let noclick = document.getElementById("no")
+    let yesclick = document.getElementById("yes")
+    popup3.classList.remove("open-popup3");
+    popup2.classList.remove("open-popup2");
+    popup1.classList.remove("open-popup1");
+
+
+    noclick.addEventListener('click', function(){
+        popup4.classList.remove("open-popup4");
+        viewlist.style.filter = "blur(0)";
+        title1.style.filter = "blur(0)";
+        isiprofile.style.filter = "blur(0)";
+        bg.style.filter = "blur(0)";
+    });
+    
+    let popup5 = document.getElementById("popup5");
+
+    yesclick.addEventListener('click',function(){
+        popup4.classList.remove("open-popup4");
+        popup5.classList.add("open-popup5");
+        let okay = document.getElementById("ok");
+        okay.addEventListener('click',function(){
+            viewlist.style.filter = "blur(0)";
+            bg.style.filter = "blur(0)";
+            isiprofile.style.filter = "blur(0)";
+            title1.style.filter = "blur(0)";
+            popup5.classList.remove("open-popup5");
+        });
+    });
+}
