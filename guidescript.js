8ec0c90cb447a1a334e130d6561cc0ea64fe648d
PemulaBanget123 <164504353+PemulaBanget123@users.noreply.github.com>
2024-04-17 21:43:35 +0700

    guidescript.js

--- guidescript.js
+++ guidescript.js
@@ -2,12 +2,16 @@ let image1 = document.getElementById("image1");
 let form1 = document.getElementById("guides");
 let form2 = document.getElementById("rules");
 let footer = document.getElementById("footer");
+let toggle = document.getElementById("toggle");
+let toggle1 = document.getElementById("toggle1");
 function leftClick(){
     btn.style.left ='0'
     form1.classList.add("open-guide");
     footer.classList.add("open-footerguide");
     form2.classList.remove("open-rules");
     footer.classList.remove("open-footerrule");
+    toggle.classList.add("changetoggle");
+    toggle1.classList.remove("changetoggle1");
     image1.style.display ='none';
 }
 function rightClick(){
@@ -16,5 +20,7 @@ function rightClick(){
     footer.classList.remove("open-footerguide");
     form2.classList.add("open-rules");
     footer.classList.add("open-footerrule");
+    toggle.classList.remove("changetoggle");
+    toggle1.classList.add("changetoggle1");
     image1.style.display ='none';
 }
