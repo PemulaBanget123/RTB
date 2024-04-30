7878abe2ec4ed0c91ed4b449bd61410e9d6e930e
PemulaBanget123 <164504353+PemulaBanget123@users.noreply.github.com>
2024-04-30 18:45:13 +0700

    Update guidescript.js

--- guidescript.js
+++ guidescript.js
@@ -1,18 +1,21 @@
-let image1 = document.getElementById("image1");
+// let image1 = document.getElementById("image1");
 let form1 = document.getElementById("guides");
 let form2 = document.getElementById("rules");
 let footer = document.getElementById("footer");
 let toggle = document.getElementById("toggle");
 let toggle1 = document.getElementById("toggle1");
+form1.classList.add("open-guide");
+footer.classList.add("open-footerguide");
 function leftClick(){
     btn.style.left ='0'
     form1.classList.add("open-guide");
     footer.classList.add("open-footerguide");
     form2.classList.remove("open-rules");
     footer.classList.remove("open-footerrule");
-    toggle.classList.add("changetoggle");
+    // toggle.classList.add("changetoggle");
+    toggle.classList.remove("changetoggle");
     toggle1.classList.remove("changetoggle1");
-    image1.style.display ='none';
+    // image1.style.display ='none';
 }
 function rightClick(){
     btn.style.left = '110px'
@@ -20,7 +23,8 @@ function rightClick(){
     footer.classList.remove("open-footerguide");
     form2.classList.add("open-rules");
     footer.classList.add("open-footerrule");
-    toggle.classList.remove("changetoggle");
+    // toggle.classList.remove("changetoggle");
+    toggle.classList.add("changetoggle");
     toggle1.classList.add("changetoggle1");
-    image1.style.display ='none';
+    // image1.style.display ='none';
 }
